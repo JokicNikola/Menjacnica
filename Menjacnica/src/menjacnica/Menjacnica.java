7e399755599bc957cc6121bfe816f08db245d48f
JokicNikola <jokic1916@fon.bg.ac.rs>
2018-03-16 01:00:27 +0100

    Implementacija interfejsa bez tela

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,9 +1,30 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
-public class Menjacnica {
+import specifikacija.Specifikacija;
+
+public class Menjacnica implements Specifikacija {
 
 	private LinkedList<Valuta> lista = new LinkedList<Valuta>();
+
+	
+	public void dodajKurs(String naziv, double kursS, double kursP, double kursK, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void izbrisiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public String vratiKurs(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
